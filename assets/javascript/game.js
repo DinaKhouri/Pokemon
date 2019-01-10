38f7ff2d9a89c0e8b054c8f09e30f35d2a6b6f2c
Dina Khouri <dina.khouri.91@gmail.com>
2019-01-10 12:15:47 -0600

    almost there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,125 +1,151 @@
 $(document).ready(function() {
-  //define characters in objects,define their current power, their next power and pictures for evolution
-
-  var pika = {
-    name: "pika",
-    power: ["10", "15", "20"]
-    // evolution: [
-    //   "assets/images/pikaicon0.png",
-    //   "assets/images/pikaicon1.png",
-    //   "assets/images/pikaicon2.png"
-    //]
-  };
-  var plant = {
-    name: "pika",
-    power: ["10", "15", "20"]
-    // evolution: [
-    //   "assets/images/planticon0.png",
-    //   "assets/images/planticon1.png",
-    //   "assets/images/planticon2.png"
-    // ]
-  };
-
-  var charm = {
-    name: "charm",
-    power: ["10", "15", "20"]
-    // evolution: [
-    //   "assets/images/charmicon0.png",
-    //   "assets/images/charmicon1.png",
-    //   "assets/images/charmicon2.png"
-    // ]
-  };
-
-  var turtle = {
-    name: "turtle",
-    power: ["10", "15", "20"]
-    // evolution: [
-    //   "assets/images/turtle1.png",
-    //   "assets/images/turtle2.png",
-    //   "assets/images/turtle3.png"
-    // ]
-  };
+  //define sounds
+  var sound_pick = document.getElementById("pick");
+  var sound_evolute = document.getElementById("evolute");
+  var sound_lose = document.getElementById("lose");
+  var sound_win = document.getElementById("win");
+  var sound_hit = document.getElementById("hit");
+  var sound_theme = document.getElementById("theme");
   //create var for when first character id selected so we can defrentiate between chosing us and choseing the enemy
   var charSelected = false;
   //create var for charachter so when it evolutes it changes the number hence loads the next evolution pic and changes power
   var characterCounter = "0";
+  //define times won to know when to end the game
+  var timesWon = "0";
+  // create life energy
+  lifeEnergy1 = "100";
+  lifeEnergy2 = "100";
   // create function to start whenever u beat a character and u get to pick a new one
-  newRound = function(winner) {
-    charSelected = true;
-    charachterCounter++;
-  };
+  // firstRound = function(winner) {
+  //   charSelected = true;
+  //   lifeEnergy1 = "100";
+  //   charachterCounter = "1";
+  //   character = "winner"; // created this to keep the character to our winning character rather than changing it when user picks the enemy
+  // };
   //when user picks first character it goes left and when we pick enemy it goes right
   //It also loads their charachter on each side based on their character counter
   //It also loads their full energy bars based on character counter
   //It also shows their hit strength based on character counter
+
   $(".icon").on("click", function(event) {
+    sound_theme.play();
     if (charSelected === false) {
-      lifeEnergy1 = "100";
-      character = $(this).attr("id"); // source https://stackoverflow.com/questions/48239/getting-the-id-of-the-element-that-fired-an-event
+      character1 = $(this).attr("id"); // source https://stackoverflow.com/questions/48239/getting-the-id-of-the-element-that-fired-an-event
       hitPower1 = $(this).attr("hitPower");
       console.log(hitPower1);
-      console.log(character);
+      console.log(character1);
       console.log(
-        "<img src= assets/images/" + character + characterCounter + ".png/>"
+        "<img src= assets/images/" + character1 + characterCounter + ".png/>"
       );
       $(".yourcharacter1")
         .children("img")
-        .attr("src", "assets/images/" + character + characterCounter + ".png"); // source for loading image https://www.sitepoint.com/community/t/load-image-onclick-with-jquery/255772/2
+        .attr("src", "assets/images/" + character1 + characterCounter + ".png"); // source for loading image https://www.sitepoint.com/community/t/load-image-onclick-with-jquery/255772/2
       charSelected = true;
-
+      sound_pick.play();
       //source for ID+Var https://stackoverflow.com/questions/743994/how-do-i-select-an-element-in-jquery-by-using-a-variable-for-the-id
-      $("#" + character).animate({
+      $("#" + character1).animate({
         left: "-370"
       });
       //load energy bar
-      $(".bar1").css({ "background-color": "yellow" });
+      $(".energyLife1").css({ "background-color": "yellow" });
     } else {
-      character = jQuery(this).attr("id");
-      lifeEnergy2 = "100";
+      character2 = jQuery(this).attr("id");
       hitPower2 = $(this).attr("hitPower");
       console.log(hitPower2);
       $(".yourcharacter2")
         .children("img")
-        .attr("src", "assets/images/" + character + characterCounter + ".png");
-      $("#" + character).animate({
+        .attr("src", "assets/images/" + character2 + "0" + ".png");
+      sound_pick.play();
+      $("#" + character2).animate({
         left: "370"
       });
-      $(".bar2").css({ "background-color": "yellow" });
+      $(".energyLife2").css({ "background-color": "yellow" });
     }
     //when user presses fight each energy bar changes according to hit power
     $(".ball").on("click", function() {
-      console.log("fight");
-      //console log them  make sure I can access them from here
-      console.log(hitPower1);
-      console.log(hitPower2);
-      console.log(lifeEnergy1);
-      console.log(lifeEnergy2);
-      lifeEnergy1 = lifeEnergy1; //- hitPower2;
-      lifeEnergy2 = lifeEnergy2 - hitPower1;
-      console.log(lifeEnergy1);
-      console.log(lifeEnergy2);
+      sound_hit.play();
+      if (timesWon > 2) {
+        sound_win.play();
+        alert("congratulations you are the strongest Pokemon!");
+      }
       //when energy bar of your character =<0 you lose
-      if (lifeEnergy1 < 0) {
+      else if (lifeEnergy1 <= 0) {
+        sound_lose.play();
         alert("you lose");
+      } else if (lifeEnergy2 <= 0) {
+        alert("pick another Enemy");
+        characterCounter++;
+        timesWon++;
+        //load your energy bar again
+        $(".energyLife1").css({ "background-color": "yellow" });
+        lifeEnergy1 = "100";
+        //load enemy's energy bar again
+        $(".energyLife2").css({ "background-color": "yellow" });
+        lifeEnergy2 = "100";
+        //load new enemy picked --> it will load from the previous lines
+        //add bounceInDown effect for evolution
+        $(".yourcharacter1")
+          .addClass("animated bounceInDown")
+          .one(
+            "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+            function() {
+              $(this).removeClass("animated bounceInDown");
+            }
+          );
+        //evolute character one based on the character counter
+        $(".yourcharacter1")
+          .children("img")
+          .attr(
+            "src",
+            "assets/images/" + character1 + characterCounter + ".png"
+          );
+        //evolution sound
+        sound_evolute.play();
+      } else if (lifeEnergy2 >= 0) {
+        console.log("fight");
+        //console log them  make sure I can access them from here
+        console.log(hitPower1);
+        console.log(hitPower2);
+        console.log(lifeEnergy1);
+        console.log(lifeEnergy2);
+        lifeEnergy1 = lifeEnergy1 - hitPower2;
+        lifeEnergy2 = lifeEnergy2 - hitPower1;
+        console.log(lifeEnergy1);
+        console.log(lifeEnergy2);
+        $(".energyLife1").css({ width: lifeEnergy1 + "%" });
+        $(".energyLife2").css({ width: lifeEnergy2 + "%" });
+        $(".ball")
+          .addClass("animated pulse")
+          .one(
+            "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+            function() {
+              $(this).removeClass("animated pulse");
+            }
+          );
+        $(".yourcharacter1")
+          .addClass("animated flash")
+          .one(
+            "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+            function() {
+              $(this).removeClass("animated flash");
+            }
+          );
+        $(".yourcharacter2")
+          .addClass("animated flash")
+          .one(
+            "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+            function() {
+              $(this).removeClass("animated flash");
+            }
+          );
       }
       //when energy bar for enemy =<0
       //1 -you get to pick another enemy to fight
       //2- your power changes
       //3-you evolute
-      if (lifeEnergy2 < 0) {
-        alert("pick another enemy");
-        lifeEnergy1 = "100";
-        characterCounter = "1";
-        $(".yourcharacter1")
-          .children("img")
-          .attr(
-            "src",
-            "assets/images/" + character + characterCounter + ".png"
-          );
-      }
     });
-  });
 
-  //repeat past 2 steps
-  // if you beat 3 characters you win the game
+    //repeat past 2 steps
+    // if you beat 3 characters you win the game
+  });
 });
