c659158b0d85b18264c0874de88b2c2d259a79b0
Dina Khouri <dina.khouri.91@gmail.com>
2019-01-10 18:45:00 -0600

    fixed some stuff yey

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,6 +28,8 @@ $(document).ready(function() {
   //It also shows their hit strength based on character counter
 
   $(".icon").on("click", function(event) {
+    lifeEnergy2 = "100";
+    $(".energyLife2").css({ width: lifeEnergy2 + "%" });
     sound_theme.play();
     if (charSelected === false) {
       character1 = $(this).attr("id"); // source https://stackoverflow.com/questions/48239/getting-the-id-of-the-element-that-fired-an-event
@@ -62,93 +64,93 @@ $(document).ready(function() {
       $(".energyLife2").css({ "background-color": "yellow" });
       //fill energy bar for both
       lifeEnergy1 = "100";
-      lifeEnergy2 = "100";
     }
-    //when user presses fight each energy bar changes according to hit power
-    $(".ball").on("click", function() {
-      sound_hit.play();
-      if (timesWon > 2) {
-        sound_win.play();
-        alert("congratulations you are the strongest Pokemon!");
-      }
-      //when energy bar of your character =<0 you lose
-      else if (lifeEnergy1 <= 0) {
-        sound_lose.play();
-        alert("you lose");
-      } else if (lifeEnergy2 <= 0) {
-        alert("pick another Enemy");
-        characterCounter++;
-        timesWon++;
-        //load your energy bar again
-        $(".energyLife1").css({ "background-color": "yellow" });
-        lifeEnergy1 = "100";
-        //load enemy's energy bar again
-        $(".energyLife2").css({ "background-color": "yellow" });
-        lifeEnergy2 = "100";
-        //load new enemy picked --> it will load from the previous lines
-        //add bounceInDown effect for evolution
-        $(".yourcharacter1")
-          .addClass("animated bounceInDown")
-          .one(
-            "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
-            function() {
-              $(this).removeClass("animated bounceInDown");
-            }
-          );
-        //evolute character one based on the character counter
-        $(".yourcharacter1")
-          .children("img")
-          .attr(
-            "src",
-            "assets/images/" + character1 + characterCounter + ".png"
-          );
-        //evolution sound
-        sound_evolute.play();
-      } else if (lifeEnergy2 >= 0) {
-        console.log("fight");
-        //console log them  make sure I can access them from here
-        console.log(hitPower1);
-        console.log(hitPower2);
-        console.log(lifeEnergy1);
-        console.log(lifeEnergy2);
-        lifeEnergy1 = lifeEnergy1 - hitPower2;
-        lifeEnergy2 = lifeEnergy2 - hitPower1;
-        console.log(lifeEnergy1);
-        console.log(lifeEnergy2);
-        $(".energyLife1").css({ width: lifeEnergy1 + "%" });
-        $(".energyLife2").css({ width: lifeEnergy2 + "%" });
-        $(".ball")
-          .addClass("animated pulse")
-          .one(
-            "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
-            function() {
-              $(this).removeClass("animated pulse");
-            }
-          );
-        $(".yourcharacter1")
-          .addClass("animated flash")
-          .one(
-            "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
-            function() {
-              $(this).removeClass("animated flash");
-            }
-          );
-        $(".yourcharacter2")
-          .addClass("animated flash")
-          .one(
-            "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
-            function() {
-              $(this).removeClass("animated flash");
-            }
-          );
-      }
-      //when energy bar for enemy =<0
-      //1 -you get to pick another enemy to fight
-      //2- your power changes
-      //3-you evolute
-    });
-
-    //repeat past 2 steps
-    // if you beat 3 characters you win the game
   });
+
+  //when user presses fight each energy bar changes according to hit power
+  $(".ball").on("click", function() {
+    sound_hit.play();
+    if (timesWon > 2) {
+      sound_win.play();
+      alert("congratulations you are the strongest Pokemon!");
+    }
+    //when energy bar of your character =<0 you lose
+    else if (lifeEnergy1 <= 0) {
+      sound_lose.play();
+      alert("you lose");
+    } else if (lifeEnergy2 <= 0) {
+      alert("pick another Enemy");
+      lifeEnergy1 = 100;
+      lifeEnergy2 = 100;
+      characterCounter++;
+      timesWon++;
+      //load your energy bar again
+      $(".energyLife1").css({ "background-color": "yellow" });
+      lifeEnergy1 = "100";
+      //load enemy's energy bar again
+      $(".energyLife2").css({ "background-color": "yellow" });
+      lifeEnergy2 = "100";
+      //load new enemy picked --> it will load from the previous lines
+      //add bounceInDown effect for evolution
+      $(".yourcharacter1")
+        .addClass("animated bounceInDown")
+        .one(
+          "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+          function() {
+            $(this).removeClass("animated bounceInDown");
+          }
+        );
+      //evolute character one based on the character counter
+      $(".yourcharacter1")
+        .children("img")
+        .attr("src", "assets/images/" + character1 + characterCounter + ".png");
+
+      //evolution sound
+      sound_evolute.play();
+    } else if (lifeEnergy2 >= 0) {
+      console.log("fight");
+      //console log them  make sure I can access them from here
+      console.log(hitPower1);
+      console.log(hitPower2);
+      console.log(lifeEnergy1);
+      console.log(lifeEnergy2);
+      lifeEnergy1 = lifeEnergy1 - hitPower2;
+      lifeEnergy2 = lifeEnergy2 - hitPower1;
+      console.log(lifeEnergy1);
+      console.log(lifeEnergy2);
+      $(".energyLife1").css({ width: lifeEnergy1 + "%" });
+      $(".energyLife2").css({ width: lifeEnergy2 + "%" });
+      $(".ball")
+        .addClass("animated pulse")
+        .one(
+          "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+          function() {
+            $(this).removeClass("animated pulse");
+          }
+        );
+      $(".yourcharacter1")
+        .addClass("animated wobble")
+        .one(
+          "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+          function() {
+            $(this).removeClass("animated wobble");
+          }
+        );
+      $(".yourcharacter2")
+        .addClass("animated wobble")
+        .one(
+          "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+          function() {
+            $(this).removeClass("animated wobble");
+          }
+        );
+    }
+    //when energy bar for enemy =<0
+    //1 -you get to pick another enemy to fight
+    //2- your power changes
+    //3-you evolute
+  });
+
+  //repeat past 2 steps
+  // if you beat 3 characters you win the game
 });
